9ce1b270f4f73765bb3826b91e915f73d01491a3
v833 <2507301541@qq.com>
2022-01-23 17:44:49 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueJsx from '@vitejs/plugin-vue-jsx'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), VueJsx()],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/styles/main.scss";'
+      }
+    }
+  }
 })
